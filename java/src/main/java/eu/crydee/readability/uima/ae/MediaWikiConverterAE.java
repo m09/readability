c6cd6d79f920ef5ab5fd68befc5d5f82edb43ee1
Mog <mog@crydee.eu>
2014-04-22 01:20:36 +0900

    fix the conversion bug by removing comments

--- java/src/main/java/eu/crydee/readability/uima/ae/MediaWikiConverterAE.java
+++ java/src/main/java/eu/crydee/readability/uima/ae/MediaWikiConverterAE.java
@@ -1,18 +1,15 @@
 package eu.crydee.readability.uima.ae;
 
 import eu.crydee.readability.uima.misc.HtmlToPlainText;
-import java.io.StringWriter;
+import java.util.regex.Pattern;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.component.ViewCreatorAnnotator;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
-import org.eclipse.mylyn.wikitext.core.parser.DocumentBuilder;
 import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
-import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
 import org.eclipse.mylyn.wikitext.mediawiki.core.MediaWikiLanguage;
 import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
 
 public class MediaWikiConverterAE extends JCasAnnotator_ImplBase {
 
@@ -24,6 +21,8 @@ public class MediaWikiConverterAE extends JCasAnnotator_ImplBase {
     @ConfigurationParameter(name = PARAM_OUT_VIEW_HTML, mandatory = true)
     private String outputHtmlViewName;
 
+    final private Pattern comments = Pattern.compile("<!--.*?-->");
+
     @Override
     public void process(JCas jcas) throws AnalysisEngineProcessException {
         JCas outputViewTxt = ViewCreatorAnnotator.createViewSafely(
@@ -32,13 +31,9 @@ public class MediaWikiConverterAE extends JCasAnnotator_ImplBase {
                 outputViewHtml = ViewCreatorAnnotator.createViewSafely(
                         jcas,
                         outputHtmlViewName);
-        StringWriter sw = new StringWriter();
-        DocumentBuilder db = new HtmlDocumentBuilder(sw, true);
-        MarkupParser markupParser = new MarkupParser(
-                new MediaWikiLanguage(),
-                db);
-        markupParser.parse(jcas.getDocumentText());
-        String html = Jsoup.clean(sw.toString(), Whitelist.relaxed());
+
+        String html = new MarkupParser(new MediaWikiLanguage()).parseToHtml(
+                comments.matcher(jcas.getDocumentText()).replaceAll(""));
         outputViewHtml.setDocumentText(html);
         outputViewTxt.setDocumentText(new HtmlToPlainText().getPlainText(
                 Jsoup.parse(html)));
