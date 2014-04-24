c37099ee908716c860d04cd6e56896267b5ca4be
Mog <mog@crydee.eu>
2014-04-24 23:25:26 +0900

    fix import

--- java/src/main/java/eu/crydee/readability/uima/cr/RevisionsCR.java
+++ java/src/main/java/eu/crydee/readability/uima/cr/RevisionsCR.java
@@ -4,7 +4,6 @@ import eu.crydee.readability.uima.ts.Revision;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
