facbf795ecbce7946c4e8c12e228a8b60ed207eb
Mog <mog@crydee.eu>
2014-05-07 14:42:21 +0900

    add a sorting stylesheet

--- java/out/res/launcher.sh
+++ java/out/res/launcher.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 
-xmllint --format dict.xml > processed/indented.xml
-xsltproc -o processed/filtered.xml scripts/filter-long-revised.xsl dict.xml
-./scripts/count.sh processed/filtered.xml > processed/filtered-count.tsv
-./scripts/count.sh dict.xml > processed/normal-count.tsv
-Rscript --vanilla 'scripts/plot.r'
+xmllint \
+    --format dict.xml \
+    > processed/indented.xml
+
+xsltproc -o processed/filtered.xml \
+    scripts/filter-long-revised.xsl \
+    dict.xml
+
+xsltproc \
+    -o processed/filtered-sorted.xml \
+    scripts/sort.xsl \
+    processed/filtered.xml
+
+xsltproc \
+    -o processed/sorted.xml \
+    scripts/sort.xsl \
+    dict.xml
+
+./scripts/count.sh \
+    processed/filtered.xml \
+    > processed/filtered-count.tsv
+
+./scripts/count.sh \
+    dict.xml \
+    > processed/normal-count.tsv
+
+Rscript \
+    --vanilla \
+    'scripts/plot.r'
---
+++ java/out/res/scripts/sort.xsl
@@ -0,0 +1,29 @@
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+  <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes"/>
+
+  <xsl:strip-space elements="*"/>
+
+  <xsl:template match="/">
+    <dict>
+      <xsl:apply-templates select="dict/original">
+        <xsl:sort select="count( revised-list/revised )"
+                  order="descending"
+                  />
+      </xsl:apply-templates>
+    </dict>
+  </xsl:template>
+
+  <xsl:template match="original">
+    <xsl:copy>
+      <xsl:apply-templates select="node()|@*"/>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="node()|@*">
+    <xsl:copy>
+      <xsl:apply-templates select="node()|@*"/>
+    </xsl:copy>
+  </xsl:template>
+</xsl:stylesheet>
