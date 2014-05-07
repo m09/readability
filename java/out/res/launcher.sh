a3a2ffc6fd3e248e78cebe1961e706dfa3243b3b
Mog <mog@crydee.eu>
2014-05-07 23:44:30 +0900

    include all the new scripts

--- java/out/res/launcher.sh
+++ java/out/res/launcher.sh
@@ -4,6 +4,21 @@ xmllint \
     --format dict.xml \
     > processed/indented.xml
 
+xsltproc \
+    -o processed/sorted.xml \
+    scripts/sort.xsl \
+    dict.xml
+
+xsltproc \
+    -o processed/dict.tsv \
+    scripts/tsv.xsl \
+    dict.xml
+
+xsltproc \
+    -o processed/dict-summary.tsv \
+    scripts/summary.xsl \
+    dict.xml
+
 xsltproc -o processed/filtered.xml \
     scripts/filter-long-revised.xsl \
     dict.xml
@@ -13,18 +28,16 @@ xsltproc \
     scripts/sort.xsl \
     processed/filtered.xml
 
-xsltproc \
-    -o processed/sorted.xml \
-    scripts/sort.xsl \
-    dict.xml
+xsltproc -o processed/filtered.tsv \
+    scripts/tsv.xsl \
+    processed/filtered.xml
 
-./scripts/count.sh \
-    processed/filtered.xml \
-    > processed/filtered-count.tsv
+xsltproc -o processed/filtered-summary.tsv \
+    scripts/summary.xsl \
+    processed/filtered.xml
 
-./scripts/count.sh \
-    dict.xml \
-    > processed/normal-count.tsv
+./scripts/count.sh "dict.xml" > "processed/dict-count.tsv"
+./scripts/count.sh "processed/filtered.xml" > "processed/filtered-count.tsv"
 
 Rscript \
     --vanilla \
