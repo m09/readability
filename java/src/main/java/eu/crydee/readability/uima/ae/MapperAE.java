10d50374600faeaff4a522e08857b6ecb95cb084
Mog <mog@crydee.eu>
2014-05-08 00:29:19 +0900

    remove useless import

--- java/src/main/java/eu/crydee/readability/uima/ae/MapperAE.java
+++ java/src/main/java/eu/crydee/readability/uima/ae/MapperAE.java
@@ -10,7 +10,6 @@ import eu.crydee.readability.uima.ts.Suggestion;
 import eu.crydee.readability.uima.ts.Token;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
