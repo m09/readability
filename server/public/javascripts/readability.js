bed7554561885ea07f9a9e1064365b90598bfd19
Mog <mog@crydee.eu>
2014-05-13 00:45:16 +0900

    still simplifying the UI code

--- server/public/javascripts/readability.js
+++ server/public/javascripts/readability.js
@@ -3,69 +3,35 @@ var Annotator = React.createClass({
     getInitialState: function() {
         return {
             data: {
-                normal: {
-                    text: "",
-                    tokens: [],
-                    annotations: []
-                },
-                filtered: {
-                    text: "",
-                    tokens: [],
-                    annotations: []
-                }
+                normal: { text: "", tokens: [], annotations: [] },
+                filtered: { text: "", tokens: [], annotations: [] }
             },
             dict: jQuery("input:radio[name='dict']:checked").val(),
             lastText: "",
-            fetched: {
-                normal: true,
-                filtered: true
-            }
+            fetched: { normal: true, filtered: true }
         };
     },
-    handleSubmit: function(dict) {
-        var text = jQuery(this.refs['#input'].getDOMNode()).text();
+    fetchAnnotations: function(dict) {
+        var text = jQuery(this.refs.input.getDOMNode()).text();
         if (text === this.state.lastText) {
-            if(this.state.fetched[dict]) {
-                return;
-            }
-        } else {
-            this.setState({
-                fetched: {
-                    normal: false,
-                    filtered: false
-                }
-            });
-        }
+            if (this.state.fetched[dict]) return;
+        } else this.setState({ fetched: { normal: false, filtered: false } });
         this.setState({lastText: text});
         jQuery.ajax({
             type: 'POST',
             url: this.props.url,
             contentType: 'application/json; charset=UTF-8',
-            data: JSON.stringify({
-                data: text,
-                dict: dict
-            })
+            data: JSON.stringify({ data: text, dict: dict })
         }).done(function(data) {
             var dataMergeable = jQuery.extend({}, this.state.data);
             dataMergeable[dict] = data;
             var fetchedMergeable = jQuery.extend({}, this.state.fetched);
             fetchedMergeable[dict] = true;
-            this.setState({
-                data: dataMergeable,
-                fetched: fetchedMergeable
-            });
+            this.setState({ data: dataMergeable, fetched: fetchedMergeable });
         }.bind(this));
     },
-    toNormal: function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.handleSubmit("normal");
-    },
-    toFiltered: function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.handleSubmit("filtered");
-    },
+    toNormal: function(e) { this.fetchAnnotations("normal"); },
+    toFiltered: function(e) { this.fetchAnnotations("filtered"); },
     render: function() {
         return (<section className="container" style={{minHeight: "300px"}}>
                 <ul className="nav nav-tabs nav-justified">
@@ -89,7 +55,7 @@ var Annotator = React.createClass({
                 </ul>
                 
                 <section className="tab-content">
-                <InputPane ref="#input"/>
+                <InputPane ref="input"/>
                 <OutputPane id="normal" data={this.state.data.normal}/>
                 <OutputPane id="filtered" data={this.state.data.filtered}/>
                 </section>
