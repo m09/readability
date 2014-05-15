95966a4ae44e840c49c6491b91bec615c83d4358
Mog <mog@crydee.eu>
2014-05-15 09:15:08 +0900

    clean leftovers of the ex client

--- server/public/javascripts/readability.js
+++
@@ -1,3 +0,0 @@
-/** @jsx React.DOM */
-React.renderComponent(<Annotator url="http://localhost:9000/"/>,
-                      document.getElementById('annotator'));
--- server/public/javascripts/widgets/InputPane.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-var InputPane = React.createClass({
-    render: function() {
-        return (<section className="tab-pane active" id="input"
-                contentEditable="true" style={{minHeight: "200px"}}>
-                this, and that!</section>);
-    }
-});
