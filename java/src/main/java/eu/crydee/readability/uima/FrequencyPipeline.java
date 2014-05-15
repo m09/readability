b0ac17b0f2ebae3b2b0cf68975672168ebe2d705
Mog <mog@crydee.eu>
2014-05-15 15:18:49 +0900

    stub work for language models estimation

---
+++ java/src/main/java/eu/crydee/readability/uima/FrequencyPipeline.java
@@ -0,0 +1,180 @@
+package eu.crydee.readability.uima;
+
+import eu.crydee.readability.uima.ae.XmiSerializerUsageAE;
+import eu.crydee.readability.uima.cr.CurrentCR;
+import eu.crydee.readability.uima.ts.Sentence;
+import eu.crydee.readability.uima.ts.Token;
+import java.util.Optional;
+import opennlp.uima.postag.POSModelResourceImpl;
+import opennlp.uima.postag.POSTagger;
+import opennlp.uima.sentdetect.SentenceDetector;
+import opennlp.uima.sentdetect.SentenceModelResourceImpl;
+import opennlp.uima.tokenize.Tokenizer;
+import opennlp.uima.tokenize.TokenizerModelResourceImpl;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.factory.ExternalResourceFactory;
+import org.apache.uima.fit.factory.TypePrioritiesFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.ExternalResourceDescription;
+
+public class FrequencyPipeline {
+
+    static private String DB_URL = null,
+            DB_USR = null,
+            DB_PW = null;
+    static private Optional<Integer> LIMIT = Optional.empty();
+
+    public static void main(String[] args) throws Exception {
+        parseArguments(args);
+
+        /* Resources descriptions */
+        ExternalResourceDescription tokenModel
+                = ExternalResourceFactory.createExternalResourceDescription(
+                        TokenizerModelResourceImpl.class,
+                        "file:opennlp/uima/models/en-token.bin");
+
+        ExternalResourceDescription sentenceModel
+                = ExternalResourceFactory.createExternalResourceDescription(
+                        SentenceModelResourceImpl.class,
+                        "file:opennlp/uima/models/en-sent.bin");
+
+        ExternalResourceDescription posModel
+                = ExternalResourceFactory.createExternalResourceDescription(
+                        POSModelResourceImpl.class,
+                        "file:opennlp/uima/models/en-pos-maxent.bin");
+
+        /* Collection reader */
+        CollectionReaderDescription crd;
+        if (LIMIT.isPresent()) {
+            crd = CollectionReaderFactory.createReaderDescription(
+                    CurrentCR.class,
+                    CurrentCR.PARAM_DB_URL, DB_URL,
+                    CurrentCR.PARAM_DB_USER, DB_USR,
+                    CurrentCR.PARAM_DB_PASSWORD, DB_PW,
+                    CurrentCR.PARAM_LIMIT, LIMIT.get());
+        } else {
+            crd = CollectionReaderFactory.createReaderDescription(
+                    CurrentCR.class,
+                    CurrentCR.PARAM_DB_URL, DB_URL,
+                    CurrentCR.PARAM_DB_USER, DB_USR,
+                    CurrentCR.PARAM_DB_PASSWORD, DB_PW);
+        }
+
+        /* Analysis engines */
+        AnalysisEngineDescription sentenceDetector
+                = AnalysisEngineFactory.createEngineDescription(
+                        SentenceDetector.class,
+                        "opennlp.uima.ModelName",
+                        sentenceModel,
+                        "opennlp.uima.SentenceType",
+                        Sentence.class.getCanonicalName());
+
+        AnalysisEngineDescription tokenizer
+                = AnalysisEngineFactory.createEngineDescription(
+                        Tokenizer.class,
+                        "opennlp.uima.ModelName",
+                        tokenModel,
+                        "opennlp.uima.SentenceType",
+                        Sentence.class.getCanonicalName(),
+                        "opennlp.uima.TokenType",
+                        Token.class.getCanonicalName());
+
+        AnalysisEngineDescription tagger
+                = AnalysisEngineFactory.createEngineDescription(
+                        POSTagger.class,
+                        "opennlp.uima.ModelName",
+                        posModel,
+                        "opennlp.uima.SentenceType",
+                        Sentence.class.getCanonicalName(),
+                        "opennlp.uima.TokenType",
+                        Token.class.getCanonicalName(),
+                        "opennlp.uima.POSFeature",
+                        "POS");
+
+        AnalysisEngineDescription consumerXmi
+                = AnalysisEngineFactory.createEngineDescription(
+                        XmiSerializerUsageAE.class,
+                        XmiSerializerUsageAE.PARAM_OUT_FOLDER,
+                        "out/cas-lm");
+
+        AggregateBuilder builder = new AggregateBuilder(
+                null,
+                TypePrioritiesFactory.createTypePriorities(
+                        Sentence.class,
+                        Token.class),
+                null);
+
+        builder.add(sentenceDetector);
+        builder.add(tokenizer);
+        builder.add(consumerXmi);
+        builder.add(tagger);
+
+        SimplePipeline.runPipeline(crd, builder.createAggregateDescription());
+    }
+
+    static private void parseArguments(String[] args) {
+        Options options = new Options();
+        options.addOption("h", "help", false, "print this message");
+        options.addOption(OptionBuilder
+                .isRequired()
+                .withLongOpt("db-url")
+                .hasArg()
+                .withArgName("jdbc:...")
+                .withDescription("The database to use to retrieve revisions "
+                        + "information. JDBC format.")
+                .create("d"));
+        options.addOption(OptionBuilder
+                .isRequired()
+                .withLongOpt("username")
+                .hasArg()
+                .withArgName("dbuser")
+                .withDescription("User to use to access the database of "
+                        + "revisions.")
+                .create("u"));
+        options.addOption(OptionBuilder
+                .isRequired()
+                .withLongOpt("password")
+                .hasArg()
+                .withArgName("pw")
+                .withDescription("Password of the user account to connect to "
+                        + "the database.")
+                .create("p"));
+        options.addOption(OptionBuilder
+                .withLongOpt("limit")
+                .hasArg()
+                .withArgName("n")
+                .withDescription("How many revisions to use. No limit means "
+                        + "all the revisions available.")
+                .create('l'));
+        CommandLineParser parser = new PosixParser();
+        @SuppressWarnings("UnusedAssignment")
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException ex) {
+            System.err.println("The CLI args could not be parsed.");
+            System.err.println("The error message was:");
+            System.err.println(" " + ex.getMessage());
+            System.exit(1);
+        }
+
+        // jdbc:postgresql://localhost:5432/readability
+        DB_URL = cmd.getOptionValue('d');
+        DB_USR = cmd.getOptionValue('u');
+        DB_PW = cmd.getOptionValue('p');
+
+        if (cmd.hasOption('l')) {
+            LIMIT = Optional.of(Integer.parseInt(cmd.getOptionValue('l')));
+        }
+    }
+}
---
+++ java/src/main/java/eu/crydee/readability/uima/cr/CurrentCR.java
@@ -0,0 +1,136 @@
+package eu.crydee.readability.uima.cr;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.UIMA_IllegalStateException;
+import org.apache.uima.UimaContext;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Level;
+import org.apache.uima.util.Logger;
+import org.apache.uima.util.Progress;
+import org.apache.uima.util.ProgressImpl;
+
+public class CurrentCR extends JCasCollectionReader_ImplBase {
+
+    private static final Logger logger = UIMAFramework.getLogger(
+            RevisionsCR.class);
+
+    public static final String PARAM_DB_URL = "REVISIONSCR_DB_URL";
+    @ConfigurationParameter(name = PARAM_DB_URL, mandatory = true)
+    private String dbUrl;
+
+    public static final String PARAM_DB_USER = "REVISIONSCR_DB_USER";
+    @ConfigurationParameter(name = PARAM_DB_USER, mandatory = true)
+    private String dbUser;
+
+    public static final String PARAM_DB_PASSWORD = "REVISIONSCR_DB_PASSWORD";
+    @ConfigurationParameter(name = PARAM_DB_PASSWORD, mandatory = true)
+    private String dbPassword;
+
+    public static final String PARAM_LIMIT = "LIMIT";
+    @ConfigurationParameter(name = PARAM_LIMIT, mandatory = false)
+    private Integer limit;
+
+    private Connection connection = null;
+    private ResultSet cursor = null;
+    private int i = 0;
+
+    @Override
+    public void initialize(UimaContext context)
+            throws ResourceInitializationException {
+        super.initialize(context);
+        try {
+            connection = DriverManager.getConnection(
+                    dbUrl,
+                    dbUser,
+                    dbPassword);
+            StringBuilder querySb = new StringBuilder();
+            querySb.append("SELECT DISTINCT ON (page_id) text FROM rev ")
+                    .append("ORDER BY page_id, timestamp DESC");
+            if (limit != null) {
+                querySb.append(" LIMIT ").append(limit);
+                logger.log(
+                        Level.INFO,
+                        "limit on the revisions to fetch set to: " + limit);
+            } else {
+                logger.log(
+                        Level.INFO,
+                        "limit on the revisions to fetch not set");
+            }
+            connection.setAutoCommit(false);
+            Statement s = connection.createStatement();
+            s.setFetchSize(5000);
+            cursor = s.executeQuery(querySb.toString());
+        } catch (SQLException ex) {
+            throw new ResourceInitializationException(ex);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            cursor.close();
+            connection.close();
+        } catch (SQLException ex) {
+            logger.log(
+                    Level.SEVERE,
+                    "couldn't close the connection to the database");
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void getNext(JCas jcas)
+            throws IOException, CollectionException {
+        try {
+            if (!cursor.next()) {
+                throw new UIMA_IllegalStateException();
+            }
+            String text = cursor.getString("text");
+            if (text == null) {
+                text = "";
+            }
+            jcas.setDocumentText(text);
+            if (++i % 10000 == 0) {
+                logger.log(Level.INFO, "processing page n°" + i);
+            }
+        } catch (SQLException ex) {
+            logger.log(
+                    Level.SEVERE,
+                    "couldn't access the database to retrieve the "
+                    + "next revision");
+            throw new CollectionException(ex);
+        }
+    }
+
+    @Override
+    public boolean hasNext()
+            throws IOException, CollectionException {
+        try {
+            return cursor.isBeforeFirst()
+                    || (cursor.getRow() != 0 && !cursor.isLast());
+        } catch (SQLException ex) {
+            logger.log(
+                    Level.SEVERE,
+                    "couldn't access the database to determine if more results "
+                    + "are available");
+            throw new CollectionException(ex);
+        }
+    }
+
+    @Override
+    public Progress[] getProgress() {
+        return new Progress[]{new ProgressImpl(i, -1, Progress.ENTITIES)};
+    }
+}
