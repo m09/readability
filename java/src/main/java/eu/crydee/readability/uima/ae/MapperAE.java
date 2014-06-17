6d2601cf90871085da45aa01c8a15473a24e1150
Mog <mog@crydee.eu>
2014-06-17 22:36:22 +0900

    reduced the use of CAS API with JCas API

--- java/src/main/java/eu/crydee/readability/uima/ae/MapperAE.java
+++ java/src/main/java/eu/crydee/readability/uima/ae/MapperAE.java
@@ -11,28 +11,30 @@ import eu.crydee.readability.uima.ts.Revised;
 import eu.crydee.readability.uima.ts.Suggestion;
 import eu.crydee.readability.uima.ts.Token;
 import eu.crydee.readability.uima.ts.TxtSuggestion;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.uima.UIMAFramework;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.CAS;
-import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.Feature;
-import org.apache.uima.cas.Type;
-import org.apache.uima.cas.TypeSystem;
-import org.apache.uima.cas.text.AnnotationFS;
-import org.apache.uima.fit.component.CasAnnotator_ImplBase;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ExternalResource;
-import org.apache.uima.fit.util.CasUtil;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Level;
+import org.apache.uima.util.Logger;
 
-public class MapperAE extends CasAnnotator_ImplBase {
+public class MapperAE extends JCasAnnotator_ImplBase {
+
+    final static private Logger logger = UIMAFramework.getLogger(
+            MapperAE.class);
 
     final static public String RES_TXT = "RES_TXT";
     @ExternalResource(key = RES_TXT)
@@ -47,15 +49,6 @@ public class MapperAE extends CasAnnotator_ImplBase {
 
     final private SetMultimap<List<String>, Pair<Mapped, Metrics>> byPos
             = HashMultimap.create();
-
-    private Type tokenT,
-            suggestionT,
-            suggestionTxtT,
-            suggestionPosT;
-
-    private Feature tokenPosF,
-            suggestionOriF,
-            suggestionRevF;
 
     @Override
     public void initialize(UimaContext context)
@@ -81,113 +74,93 @@ public class MapperAE extends CasAnnotator_ImplBase {
     }
 
     @Override
-    public void typeSystemInit(TypeSystem aTypeSystem)
-            throws AnalysisEngineProcessException {
-        super.typeSystemInit(aTypeSystem);
-        tokenT = aTypeSystem.getType(Token.class.getCanonicalName());
-        tokenPosF = tokenT.getFeatureByBaseName("POS");
-        suggestionT = aTypeSystem.getType(Suggestion.class.getCanonicalName());
-        suggestionPosT
-                = aTypeSystem.getType(PosSuggestion.class.getCanonicalName());
-        suggestionTxtT
-                = aTypeSystem.getType(TxtSuggestion.class.getCanonicalName());
-        suggestionOriF = suggestionT.getFeatureByBaseName("original");
-        suggestionRevF = suggestionT.getFeatureByBaseName("revised");
-    }
-
-    @Override
-    public void process(CAS aCas) throws AnalysisEngineProcessException {
-        List<AnnotationFS> tokens
-                = new ArrayList<>(CasUtil.select(aCas, tokenT));
+    public void process(JCas jcas) throws AnalysisEngineProcessException {
+        List<Token> tokens
+                = new ArrayList<>(JCasUtil.select(jcas, Token.class));
         List<String> txtTokens = tokens.stream()
-                .map(t -> t.getCoveredText())
+                .map(Token::getCoveredText)
                 .collect(Collectors.toList()),
                 posTokens = tokens.stream()
-                .map(t -> t.getFeatureValueAsString(tokenPosF))
+                .map(Token::getPOS)
                 .collect(Collectors.toList());
-        AnnotationFS[] txtTokensArray = tokens.stream()
-                .map(t -> aCas.createAnnotation(
-                                tokenT,
+        Token[] txtTokensArray = tokens.stream()
+                .map(t -> new Token(jcas,
                                 t.getBegin(),
                                 t.getEnd()))
                 .collect(Collectors.toList())
-                .toArray(new AnnotationFS[0]),
-                posTokensArray = tokens.stream()
-                .map(t -> {
-                    AnnotationFS ann = aCas.createAnnotation(
-                            tokenT,
-                            t.getBegin(),
-                            t.getEnd());
-                    ann.setFeatureValueFromString(
-                            tokenPosF,
-                            t.getFeatureValueAsString(tokenPosF));
-                    return ann;
-                })
-                .collect(Collectors.toList())
-                .toArray(new AnnotationFS[0]);
+                .toArray(new Token[0]),
+                posTokensArray = tokens.toArray(new Token[0]);
         List<Pair<Integer, Integer>> positions = tokens.stream()
                 .map(t -> Pair.of(t.getBegin(), t.getEnd()))
                 .collect(Collectors.toList());
-        String text = aCas.getDocumentText();
-        try {
-            annotateFromMap(
-                    text,
-                    aCas,
-                    suggestionTxtT,
-                    positions,
-                    txtTokens,
-                    txtTokensArray,
-                    byTxt);
-            annotateFromMap(
-                    text,
-                    aCas,
-                    suggestionPosT,
-                    positions,
-                    posTokens,
-                    posTokensArray,
-                    byPos);
-        } catch (CASException ex) {
-            throw new AnalysisEngineProcessException(ex);
-        }
+        String text = jcas.getDocumentText();
+        annotateFromMap(
+                text,
+                jcas,
+                TxtSuggestion.class,
+                positions,
+                txtTokens,
+                txtTokensArray,
+                byTxt);
+        annotateFromMap(
+                text,
+                jcas,
+                PosSuggestion.class,
+                positions,
+                posTokens,
+                posTokensArray,
+                byPos);
     }
 
     private void annotateFromMap(
             String text,
-            CAS aCas,
-            Type suggestionT,
+            JCas jcas,
+            Class<? extends Suggestion> suggestionClass,
             List<Pair<Integer, Integer>> positions,
             List<String> tokens,
-            AnnotationFS[] annotations,
+            Token[] annotations,
             SetMultimap<List<String>, Pair<Mapped, Metrics>> m)
-            throws CASException {
+            throws AnalysisEngineProcessException {
         for (List<String> suggestionTokens : m.keySet()) {
             int width = suggestionTokens.size();
             for (Integer i : getSublistIndices(tokens, suggestionTokens)) {
                 int begin = positions.get(i).getLeft(),
                         end = positions.get(i + width - 1).getRight();
-                AnnotationFS suggestion = aCas.createAnnotation(
-                        suggestionT,
-                        begin,
-                        end);
-                Original original = new Original(aCas.getJCas(), begin, end);
-                FSArray fsa = new FSArray(aCas.getJCas(), width);
+
+                Suggestion suggestion = null;
+                try {
+                    suggestion = suggestionClass
+                            .getConstructor(JCas.class, int.class, int.class)
+                            .newInstance(jcas, begin, end);
+                } catch (NoSuchMethodException |
+                        SecurityException |
+                        InstantiationException |
+                        IllegalAccessException |
+                        IllegalArgumentException |
+                        InvocationTargetException ex) {
+                    logger.log(
+                            Level.SEVERE,
+                            "coudln't instantiate a suggestion annotation",
+                            ex);
+                    throw new AnalysisEngineProcessException(ex);
+                }
+                Original original = new Original(jcas, begin, end);
+                FSArray fsa = new FSArray(jcas, width);
                 fsa.copyFromArray(annotations, i, 0, width);
                 original.setTokens(fsa);
                 original.setText(text.substring(begin, end));
-                suggestion.setFeatureValue(suggestionOriF, original);
-                Set<Pair<Mapped, Metrics>> revisedSet
-                        = m.get(suggestionTokens);
-                FSArray revisedArray
-                        = new FSArray(aCas.getJCas(), revisedSet.size());
+                suggestion.setOriginal(original);
+                Set<Pair<Mapped, Metrics>> revisedSet = m.get(suggestionTokens);
+                suggestion.setRevised(new FSArray(jcas, revisedSet.size()));
                 int s = 0;
                 for (Pair<Mapped, Metrics> pair : revisedSet) {
                     Mapped rev = pair.getKey();
                     int revisedWidth = rev.getTokens().size();
-                    Revised revised = new Revised(aCas.getJCas(), begin, end);
+                    Revised revised = new Revised(jcas, begin, end);
                     revised.setCount(pair.getValue().count);
                     revised.setScore(pair.getValue().score);
                     StringArray sa
-                            = new StringArray(aCas.getJCas(), revisedWidth);
+                            = new StringArray(jcas, revisedWidth);
                     sa.copyFromArray(
                             rev.getTokens().toArray(new String[0]),
                             0,
@@ -195,10 +168,9 @@ public class MapperAE extends CasAnnotator_ImplBase {
                             revisedWidth);
                     revised.setTokens(sa);
                     revised.setText(rev.getText());
-                    revisedArray.set(s++, revised);
+                    suggestion.setRevised(s++, revised);
                 }
-                suggestion.setFeatureValue(suggestionRevF, revisedArray);
-                aCas.addFsToIndexes(suggestion);
+                jcas.addFsToIndexes(suggestion);
             }
         }
 
