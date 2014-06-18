21ef42fac38349525acb78569f02f7d3647f9166
Mog <mog@crydee.eu>
2014-06-18 14:42:36 +0900

    add a set method to our interface to enable us to use it as a data structure to carry rewritings information at runtime, not only to expose our resource

--- java/src/main/java/eu/crydee/readability/uima/res/ReadabilityDict.java
+++ java/src/main/java/eu/crydee/readability/uima/res/ReadabilityDict.java
@@ -13,6 +13,12 @@ public interface ReadabilityDict {
 
     public void add(Mapped original, Mapped revised, Integer count);
 
+    public void set(
+            Mapped original,
+            Mapped revised,
+            Integer count,
+            Double score);
+
     public Optional<Map<Mapped, Metrics>> getRevisions(Mapped original);
 
     public Set<Mapped> keySet();
