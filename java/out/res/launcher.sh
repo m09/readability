0bb5c197a7b6b8e2e17641f416bb6f7f85fe6239
Mog <mog@crydee.eu>
2014-07-01 15:39:12 +0900

    adapt the post processor to the new lack of pos resource creation

--- java/out/res/launcher.sh
+++ java/out/res/launcher.sh
@@ -1,40 +1,38 @@
 #!/usr/bin/env bash
 
 rm -rf processed
+d=Txt
+for t in 'full' 'filtered'; do
 
-for d in 'Txt' 'Pos'; do
-    for t in 'full' 'filtered'; do
+    bd="processed/$t"
+    bf="$t$d.xml"
 
-        bd="processed/$d/$t"
-        bf="$t$d.xml"
+    mkdir -p "$bd"
 
-        mkdir -p "$bd"
+    xmllint \
+        --format "$bf" \
+        > "$bd/indented.xml"
 
-        xmllint \
-            --format "$bf" \
-            > "$bd/indented.xml"
+    xsltproc \
+        -o "$bd/sorted.xml" \
+        scripts/sort.xsl \
+        "$bf"
 
-        xsltproc \
-            -o "$bd/sorted.xml" \
-            scripts/sort.xsl \
-            "$bf"
+    xsltproc \
+        -o "$bd/dict.tsv" \
+        scripts/tsv.xsl \
+        "$bf"
 
-        xsltproc \
-            -o "$bd/dict.tsv" \
-            scripts/tsv.xsl \
-            "$bf"
+    xsltproc \
+        -o "$bd/dict-summary.tsv" \
+        scripts/summary.xsl \
+        "$bf"
 
-        xsltproc \
-            -o "$bd/dict-summary.tsv" \
-            scripts/summary.xsl \
-            "$bf"
+    # ./scripts/count.sh "$d" > "processed/$d/dict-count.tsv"
+    # ./scripts/count.sh "processed/$d/filtered.xml" > "processed/$d/filtered-count.tsv"
 
-        # ./scripts/count.sh "$d" > "processed/$d/dict-count.tsv"
-        # ./scripts/count.sh "processed/$d/filtered.xml" > "processed/$d/filtered-count.tsv"
-
-        # Rscript \
-        #     --vanilla \
-        #     'scripts/plot.r' \
-        #     "$d"
-    done
+    # Rscript \
+    #     --vanilla \
+    #     'scripts/plot.r' \
+    #     "$d"
 done
