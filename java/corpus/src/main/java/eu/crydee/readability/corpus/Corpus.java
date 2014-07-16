8d2739322968516343043c208b3ac96a0e108e6d
Mog <mog@crydee.eu>
2014-07-17 02:12:36 +0900

    centralize everything in a multi-module project

---
+++ java/corpus/src/main/java/eu/crydee/readability/corpus/Corpus.java
@@ -0,0 +1,14 @@
+package eu.crydee.readability.corpus;
+
+/**
+ * Class containing corpus metadata.
+ *
+ * @author mog
+ */
+public class Corpus {
+
+    /**
+     * Path to the corpus in the jar.
+     */
+    static public final String path = "eu/crydee/readability/corpus/corpus.xml";
+}
